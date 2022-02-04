750d18883099dad24ee03a76b9bed533f02e3ba0
Marco Chiapello <chiapello.m@gmail.com>
2022-02-04 10:37:13 +0100

    update

--- _deploy.sh
+++ _deploy.sh
@@ -3,10 +3,10 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "main" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "chiapello.m@gmail.com"
+git config --global user.name "Marco Chiapello"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
